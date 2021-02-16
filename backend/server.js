cd938e192eddd9cf117bc54f8edd398975362d80
Anna Kijania <kijania.anna@gmail.com>
2021-02-16 14:31:25 +0100

    Remove dotenv

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-require('dotenv').config();
 
 const productsRoutes = require('./routes/products.routes');
 const ordersRoutes = require('./routes/orders.routes');
